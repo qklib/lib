c5c0e1f066ac9d89dddeb34839a0fd43ab42cf04
linux_subscribe <linux_subscribe@126.com>
2017-03-31 10:41:02 +0800

    add select function into BaseService.

--- Engine/src/main/java/net/javahub/service/BaseService.java
+++ Engine/src/main/java/net/javahub/service/BaseService.java
@@ -1,10 +1,36 @@
 package net.javahub.service;
 
+import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class BaseService<T> {
+import net.javahub.dao.IBaseDao;
 
+@Service
+public abstract class BaseService<T, K extends Serializable, E> {
+
+	@Autowired
+	private IBaseDao<T,K,E> baseDao;
+	
 	public BaseService(){
+	}
+	
+	public abstract E getExampleForSelectList();
+	
+	public List<T> selectListByExample(Class<T> objectClass) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		E example = getExampleForSelectList();
+		List<T> list=null;
+		list=baseDao.selectByExample(objectClass, example);
+		return list;
+	}
+	
+	public List<T> selectListByExampleWithRowBounds(Class<T> objectClass, int offset, int limit) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		E example = getExampleForSelectList();
+		List<T> list=null;
+		list=baseDao.selectByExampleWithRowbounds(objectClass, example, offset, limit);
+		return list;
 	}
 }
--- Engine/src/main/java/net/javahub/service/BaseTransactionalService.java
+++ Engine/src/main/java/net/javahub/service/BaseTransactionalService.java
@@ -1,9 +1,11 @@
 package net.javahub.service;
 
+import java.io.Serializable;
+
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly=false, rollbackFor=Exception.class, propagation=Propagation.REQUIRED)
-public class BaseTransactionalService<T> extends BaseService<T>{
+public abstract class BaseTransactionalService<T, K extends Serializable, E> extends BaseService<T, K, E>{
 
 }
