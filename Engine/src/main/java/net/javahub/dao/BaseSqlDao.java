7bb0e4150c093c208488237cac66d5072aeacd72
linux_subscribe <linux_subscribe@126.com>
2017-04-07 17:10:05 +0800

    To enable select results being mapped to specified class.

--- Engine/src/main/java/net/javahub/dao/BaseSqlDao.java
+++ Engine/src/main/java/net/javahub/dao/BaseSqlDao.java
@@ -1,12 +1,21 @@
 package net.javahub.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ResultMap;
+import org.apache.ibatis.mapping.ResultMapping;
+import org.apache.ibatis.mapping.SqlCommandType;
+import org.apache.ibatis.mapping.SqlSource;
+import org.apache.ibatis.scripting.LanguageDriver;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 @Repository
 public class BaseSqlDao {
@@ -14,11 +23,50 @@ public class BaseSqlDao {
 	@Autowired
 	private SqlSession sqlSession;
 	
+	private static final String DEFAULT_MS_ID="default_ms_id";
+	
+	public String addMappedStatementToConfiguration(final String msId, final String defaultMsId, final Class<?> resultType, final String sql, final Object param) {
+		final Configuration configuration=sqlSession.getConfiguration();
+		String actualMsId=msId;
+		if(StringUtils.isEmpty(actualMsId)){
+			actualMsId=defaultMsId;
+		}
+		if(!configuration.hasStatement(actualMsId, false)){
+			LanguageDriver languageDriver=configuration.getDefaultScriptingLanguageInstance();
+			SqlSource sqlSource=languageDriver.createSqlSource(configuration, sql, param==null?null:param.getClass());
+			List<ResultMapping> resultMappings=new ArrayList<ResultMapping>();
+			List<ResultMap> resultMaps=new ArrayList<ResultMap>();
+			ResultMap.Builder resultMapBuilder=new ResultMap.Builder(
+					configuration, 
+					actualMsId, 
+					resultType, 
+					resultMappings);
+			resultMaps.add(resultMapBuilder.build());
+			MappedStatement ms = new MappedStatement.Builder(configuration, actualMsId, sqlSource, SqlCommandType.SELECT)
+		    		.resultMaps(resultMaps)
+		    		.build();
+		    configuration.addMappedStatement(ms);	
+		}
+		return actualMsId;
+	}
+	
 	public List<Map<String, Object>> select(SqlWrapper sqlWrapper) {
 		IBaseSqlMapper dao=sqlSession.getMapper(IBaseSqlMapper.class);
 		return dao.select(sqlWrapper);
 	}
 
+	public <E> E selectOne(SqlWrapper sqlWrapper, Class<E> resultType){
+		String msId=String.valueOf(sqlWrapper.getSqlStatement().hashCode());
+		String actualMsId=addMappedStatementToConfiguration(msId, DEFAULT_MS_ID, resultType, sqlWrapper.getSqlStatement(), sqlWrapper.getParams());
+		return sqlSession.selectOne(actualMsId, sqlWrapper.getParams());
+	}
+	
+	public <E> List<E> selectList(SqlWrapper sqlWrapper, Class<E> resultType){
+		String msId=String.valueOf(sqlWrapper.getSqlStatement().hashCode());
+		String actualMsId=addMappedStatementToConfiguration(msId, DEFAULT_MS_ID, resultType, sqlWrapper.getSqlStatement(), sqlWrapper.getParams());
+		return sqlSession.selectList(actualMsId, sqlWrapper.getParams());
+	}
+	
 	public static class SqlWrapper{
 		private Map<String, Object> params;
 		private String sqlStatement;
@@ -51,5 +99,10 @@ public class BaseSqlDao {
 		public Object removeParam(String key){
 			return params.remove(key);
 		}
+		
+		public void clearParam(){
+			params.clear();
+		}
 	}
+
 }
