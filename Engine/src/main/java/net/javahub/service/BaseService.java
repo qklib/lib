5e2671f30d2981f5cab792679e985eb3cf4a32f5
linux_subscribe <linux_subscribe@126.com>
2017-04-10 10:45:21 +0800

    base daos should be 'protected'.

--- Engine/src/main/java/net/javahub/service/BaseService.java
+++ Engine/src/main/java/net/javahub/service/BaseService.java
@@ -4,16 +4,21 @@ import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import net.javahub.dao.IBaseDao;
+import net.javahub.dao.IBaseDaoWithBlobs;
 
 @Service
 public class BaseService<T, K extends Serializable, E> {
 
 	@Autowired
-	private IBaseDao<T,K,E> baseDao;
+	protected IBaseDao<T,K,E> baseDao;
+	
+	@Autowired 
+	protected IBaseDaoWithBlobs<T,K,E> baseDaoWithBlogs;
 	
 	public BaseService(){
 	}
@@ -27,6 +32,18 @@ public class BaseService<T, K extends Serializable, E> {
 	protected List<T> selectListByExampleWithRowBounds(Class<T> objectClass, E example, int offset, int limit) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
 		List<T> list=null;
 		list=baseDao.selectByExampleWithRowbounds(objectClass, example, offset, limit);
+		return list;
+	}
+	
+	protected List<T> selectListByExampleWithBlobs(Class<T> objectClass, E example) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		List<T> list=null;
+		list=baseDaoWithBlogs.selectByExampleWithBLOBs(objectClass, example);
+		return list;
+	}
+	
+	protected List<T> selectListByExampleWithRowBoundsWithBlobs(Class<T> objectClass, E example, int offset, int limit) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		List<T> list=null;
+		list=baseDaoWithBlogs.selectByExampleWithBLOBsWithRowbounds(objectClass, example, new RowBounds(offset, limit));
 		return list;
 	}
 }
